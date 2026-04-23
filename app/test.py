0f698eb814248cf108e596ec282c3d7b9d434cc8
JuvenalMunoz <juvenalmaster@gmail.com>
2026-04-23 18:39:00 -0400

    avance modulo 2 seguridad

--- app/test.py
+++ app/test.py
@@ -1,4 +1,10 @@
-import os
+""" import os
 
 user_input = input("Enter command: ")
-os.system(user_input)
+os.system(user_input) """
+
+
+import subprocess
+
+command = ["ls"]
+subprocess.run(command)
