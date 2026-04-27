339567505a4e3c5a6af6c5ff1530fc5d96d4b543
Juvenal Muñoz <juvenalmaster@gmail.com>
2026-04-27 13:16:55 -0400

    Update scan.sh

--- security/scan.sh
+++ security/scan.sh
@@ -2,11 +2,12 @@
 
 echo "🔐 Running security scan..."
 
-/usr/local/bin/bandit -r app/
+python3 -m bandit -r app/
 
 if [ $? -ne 0 ]; then
-    echo "❌ Vulnerabilities detected!"
-    exit 1
+  echo "❌ Vulnerabilities detected!"
+  exit 1
 else
-    echo "✅ No critical vulnerabilities"
+  echo "✅ No critical vulnerabilities"
 fi
+
